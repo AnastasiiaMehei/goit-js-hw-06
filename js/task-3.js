dd98e78d2531c0999bab5574f91948e49b80c671
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-02-17 20:55:03 +0200

    2

---
+++ js/task-3.js
@@ -0,0 +1,27 @@
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = value;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    return this.#value.push(str);
+  }
+  padStart(str) {
+    return this.#value.unshift(str);
+  }
+  padBoth(str) {
+    return this.#value.concat(str);
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
